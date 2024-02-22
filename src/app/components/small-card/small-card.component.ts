4f22b3ff4256cbd11648c82e09af0690b5712598
ferricco <fcgricco@outlook.com>
2024-02-21 23:45:06 -0300

    content created

---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-small-card',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.css'
+})
+export class SmallCardComponent {
+
+  @Input()
+  photoCover:string =""
+  @Input()
+  cardTitle:string =""
+  @Input()
+  Id:string="0"
+}
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Como a Apple domina o Mercado",
+        "description": "Ficamos impressionados como celulares são lançados em pouquissimo tempo, mas como a Apple consegue se manter no mercado com clientes tão fiés?",
+        "photoCover":"https://classic.exame.com/wp-content/uploads/2020/09/GettyImages-1227865723-1.jpg?quality=70&strip=info&w=1024"
+    },
+    {
+        "id":"2",
+        "title":"Como suas redes sociais protegidas",
+        "description":"Dicas de como você pode proteger suas redes sociais com passos simples",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIEHlSUUrVukjB07sg_eoxYLhTlb_phIFRyQ&usqp=CAU"
+    },
+    {
+        "id":"3",
+        "title":"Saiba quais as linguagens que estão em alta e não fique fora do mercado",
+        "description":"Hoje com uma vasta diversidade de linguagens de programação, saber como cada uma funciona deve ser um desafio, e fica dificil saber quais usar e em que circunstancia usar",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5MjvFgcciZ2gtRNS4YU4ImX_oFQLRyQAfHw&usqp=CAU"
+    },
+    {
+        "id":"4",
+        "title":"Recomendações de livros para te ajudar a evoluir",
+        "description":"Talvez seja um desafio para você criar uma rotina de leitura, mas mostrarei como a leitura é importante e como ela pode mudar a sua vida",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGXzTuAc8icmMYOcepoR_S7m4bRR8jbQPwyA&usqp=CAU"
+    }
+]
